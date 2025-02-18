c4adf87fc094f6c39420b3841868447d45f0fe18
Cyril <guilouz6@hotmail.com>
2025-02-18 23:54:14 +0100

    remove heatbreak

--- klippy/webhooks.py
+++ klippy/webhooks.py
@@ -5,7 +5,6 @@
 # This file may be distributed under the terms of the GNU GPLv3 license
 import logging, socket, os, sys, errno, json, collections
 import gcode
-import extras.heaters as heater # FLSUN Changes
 
 REQUEST_LOG_SIZE = 20
 
