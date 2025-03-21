11ddccef1e0e591551f3adc643ff5fd778d74fdf
Cyril <guilouz6@hotmail.com>
2025-03-21 18:33:23 +0100

    Improve logic of bed heating

--- klippy/extras/heater_bed.py
+++ klippy/extras/heater_bed.py
@@ -19,30 +19,22 @@ class PrinterHeaterBed:
         # Set Bed Temperature
         temp = gcmd.get_float('S', 0.)
         # Start FLSUN Changes
-        hotbed_0 = gcmd.get_float('A', 1)
-        hotbed_1 = gcmd.get_float('B', 1)
+        hotbed = gcmd.get_float('B', -1)
         # End FLSUN Changes
         pheaters = self.printer.lookup_object('heaters')
         # Start FLSUN Changes
         #pheaters.set_temperature(self.heater, temp, wait)
         gcode = self.printer.lookup_object('gcode')
+        if hotbed == 0 or hotbed == -1:
+            pheaters.set_temperature(self.heater, temp, False)
+        if hotbed == 1 or hotbed == -1:
+            gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=heater_bed_2 TARGET=%f WAIT=0" % temp)
+        
         if wait:
-            if hotbed_0 == 0:
-                pheaters.set_temperature(self.heater, temp, False)
-            if hotbed_1 == 0:
-                gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=heater_bed_2 TARGET=%f WAIT=0" % temp)
-        if hotbed_0==1:
-            pheaters.set_temperature(self.heater, temp, wait)
-        if hotbed_1==1:
-            if wait:
-                gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=heater_bed_2 TARGET=%f WAIT=1" % temp) 
-            else:
-                gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=heater_bed_2 TARGET=%f WAIT=0" % temp)
-        if wait:
-            if hotbed_0 == 0:
-                pheaters.set_temperature(self.heater, 0, False)
-            if hotbed_1 == 0:
-                gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=heater_bed_2 TARGET=%f WAIT=0" % 0)
+            if hotbed == 0 or hotbed == -1:
+                pheaters.set_temperature(self.heater, temp, True)
+            if hotbed == 1 or hotbed == -1:
+                gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=heater_bed_2 TARGET=%f WAIT=1" % temp)
         # End FLSUN Changes
     def cmd_M190(self, gcmd):
         # Set Bed Temperature and Wait
