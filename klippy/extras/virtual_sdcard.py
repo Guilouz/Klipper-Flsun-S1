b40ef17d504797f4f02f7041baefb72902270ad4
Cyril <guilouz6@hotmail.com>
2025-03-28 11:23:03 +0100

    Improvements

--- klippy/extras/virtual_sdcard.py
+++ klippy/extras/virtual_sdcard.py
@@ -129,6 +129,7 @@ class VirtualSD:
             self.current_file.close()
             self.current_file = None
             self.print_stats.note_cancel()
+            self.gcode.run_script_from_command("_END_PRINT") # FLSUN Changes
         self.file_position = self.file_size = 0
     # G-Code commands
     def cmd_error(self, gcmd):
