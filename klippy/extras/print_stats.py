6be66d54dd09e7f2a82794d1fd16a97ef176798a
Cyril <guilouz6@hotmail.com>
2025-06-01 09:59:13 +0200

    print_stats: Fix for filament statistics bug  in print_stats.py for toolchangers

--- klippy/extras/print_stats.py
+++ klippy/extras/print_stats.py
@@ -17,7 +17,7 @@ class PrintStats:
             "SET_PRINT_STATS_INFO", self.cmd_SET_PRINT_STATS_INFO,
             desc=self.cmd_SET_PRINT_STATS_INFO_help)
         printer.register_event_handler("extruder:activate_extruder",
-                                       self._handle_activate_extruder)Add commentMore actions
+                                       self._handle_activate_extruder)
     def _handle_activate_extruder(self):
         gc_status = self.gcode_move.get_status()
         self.last_epos = gc_status['position'].e
